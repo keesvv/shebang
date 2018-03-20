06636cd1aca5171233320527b022f3d101fa8aa5
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-03-20 19:41:59 +0100

    Added sample JSON skeleton

--- shebang.sh
+++ shebang.sh
@@ -3,7 +3,7 @@ if [[ "$1" = "" ]]; then
   echo "Error: You must specify a GitHub username and repository."
   echo -e "Format: \e[96mshebang <username>/<repository> [branch]\e[0m"
   echo "Example:"
-  echo -e "    \e[96mshebang keesvv/plutonium-syntax\e[0m"
+  echo -e "    \e[96mshebang keesvv/shebang\e[0m"
   exit
 fi
 
@@ -18,9 +18,13 @@ function log {
   echo -e "\e[96m>\e[0m $1"
 }
 
+function err {
+  echo -e "\e[91m[!]\e[0m $1"
+}
+
 # Set local variables
 repository="$1"
-install_script="https://raw.githubusercontent.com/$repository/$branch/shebang/"
+install_script="https://raw.githubusercontent.com/$repository/$branch/shebang.json"
 
 # Clear the screen
 clear
@@ -28,4 +32,16 @@ clear
 # Get the install descriptor
 echo SHEBANG | toilet
 log "Getting install descriptor..."
-descriptor=$(curl -fsSL "$install_script")
+descriptor=$(curl -fs --output /dev/stderr --write-out "%{http_code}" "$install_script")
+
+# Check if a common error code is returned
+if [[ "$descriptor" -ne 200 ]]; then
+  if [[ "$descriptor" = 404 ]]; then
+    err "The shebang.json file could not be found in this repository."
+    err "Please report this to the author of the repository."
+    err "If you are the author, see github.com/keesvv/shebang for more info."
+  fi
+else
+  log "Install descriptor found!"
+  echo "$descriptor"
+fi
