b0ca38bc0619e2610d3dd7acf2a6334e5d614643
Kees van Voorthuizen <kees.van.voorthuizen@gmail.com>
2018-06-10 12:55:11 +0200

    Added simplified methods

--- Shebang/Utils.cs
+++ Shebang/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 
@@ -13,11 +14,7 @@ namespace Shebang
 
         public static void ForceDeleteDirectory(string directoryPath)
         {
-            if (!Directory.Exists(directoryPath))
-            {
-                return;
-            }
-
+            if (!Directory.Exists(directoryPath)) return;
             NormalizeAttributes(directoryPath);
             Directory.Delete(directoryPath, true);
         }
@@ -38,6 +35,66 @@ namespace Shebang
             }
 
             File.SetAttributes(directoryPath, FileAttributes.Normal);
+        }
+
+        public static void CrossPlatformAction(Action windowsAction, Action unixAction)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.Win32NT:
+                case PlatformID.WinCE:
+                    // Windows-based systems
+                    windowsAction.Invoke();
+                    break;
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                    // Unix-based systems
+                    unixAction.Invoke();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static void RunShellCommand(string command, bool showOutput = true)
+        {
+            Process process = new Process();
+            CrossPlatformAction(
+                () =>
+                {
+                    if (string.IsNullOrWhiteSpace(command)) return;
+                    process.StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = "powershell",
+                        Arguments = "-Command \"" + command + "\"",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
+                    };
+                },
+                
+                () =>
+                {
+                    if (string.IsNullOrWhiteSpace(command)) return;
+                    process.StartInfo = new ProcessStartInfo()
+                    {
+                        FileName = "bash",
+                        Arguments = "-c \"" + command + "\"",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true
+                    };
+                });
+
+            process.OutputDataReceived += (o, ev) =>
+            {
+                Console.WriteLine(ev.Data);
+            };
+
+            process.Start();                
+            if (showOutput) process.BeginOutputReadLine();
+
+            process.WaitForExit();
         }
     }
 }
